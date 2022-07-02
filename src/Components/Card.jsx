99a247ec276eae67ee0fd1e94d39c403801256a5
Arif <kanebrian0634@gmail.com>
2022-07-02 10:59:31 +0300

    ..

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,44 +1,53 @@
-
-import './Card.css';
+import "./Card.css";
+import loca from "../Assets/loca.png";
+import mai from "../Assets/mai.png";
+import pho from "../Assets/pho.png";
 
 const Card = (props) => {
- 
-    const {
-        picture: { medium },
-        name: { title, first, last },
-        email,
-        cell,
-        location: { city, country },
-        dob: { age },
-        registered: { date },
-      } = props;
-
+  const {
+    picture: { medium },
+    name: { title, first, last },
+    email,
+    cell,
+    location: { city, country },
+    dob: { age },
+    registered: { date },
+  } = props;
 
   return (
-    <div className='container'>
+    <div className="container">
+      <div className="card-header">
+        <img className="pic" src={medium} alt="" />
+        <p className="name">
+          {title} {first} {last}
+        </p>
+      </div>
 
-        <div className='card-left'>
-           <img src={medium} alt="" />
-           <i>mail</i>
-           <i>tel</i>
-           <i>loc</i>
+      <div className="user-info">
 
-
-        </div>
-
-        <div className='card-right'>
-            <p className='name' >{title} {first} {last}</p>
-            <p className='email'>{email}</p>
-            <p className='cell'>{cell}</p>
-            <p className='location'>{city} - {country}</p>
+        <div className="card-icons">
+          <img src={mai} alt="mail-icon" />
+          <img src={pho} alt="phone-icon" />
+          <img src={loca} alt="location-icon" />
 
         </div>
 
-        
-        
+        <div className="card-info">
+          <p className="email">{email}</p>
+          <p className="cell">{cell}</p>
+          <p className="location">
+            {city} - {country}
+          </p>
+        </div>
+      </div>
+
+      <div className="footer-info">
+        <p className="age">Age : {age}</p>
+        <p className="date">Registere Date : {date}</p>
+      </div>
 
     </div>
-  )
-}
+  );
+};
 
 export default Card;
